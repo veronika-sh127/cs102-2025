3b2f5f6248b9821729979d9d58f205e87b470d4a
veronika-sh127 <ronilodge01@gmail.com>
2025-10-22 21:59:08 +0300

    Реализованы функции шифра Видженера

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,8 +9,21 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    index = 0
+    for char in plaintext:
+        if char.isalpha():
+            if char.isupper():
+                base = ord("A")
+                shift = ord(keyword[index % len(keyword)]) - base
+            else:
+                base = ord("a")
+                shift = ord(keyword[index % len(keyword)]) - base
+            ciphertext += chr((ord(char) - base + shift) % 26 + base)
+            index +=1
+        else:
+            ciphertext += char
     return ciphertext
+
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
@@ -24,5 +37,19 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    index = 0
+    for char in ciphertext:
+        if char.isalpha():
+            if char.isupper():
+                base = ord("A")
+                shift = ord(keyword[index % len(keyword)]) - base
+            else:
+                base = ord("a")
+                shift = ord(keyword[index % len(keyword)]) - base
+            plaintext += chr((ord(char) - base - shift) % 26 + base)
+            index += 1
+        else:
+            plaintext += char
     return plaintext
+
+print(decrypt_vigenere('LXFOPVEFRNHR', "LEMON"))
