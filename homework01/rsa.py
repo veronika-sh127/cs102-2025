39828e9d747af0f46e9d9ac4accdc2db09c18a05
veronika-sh127 <ronilodge01@gmail.com>
2025-11-03 22:53:46 +0300

    Исправлены ошибки

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -42,9 +42,9 @@ def multiplicative_inverse(e: int, phi: int) -> int:
     """
 
     def extended_gcd(a: int, b: int) -> tp.Tuple[int, int, int]:
-        '''
+        """
         Euclid's extended algorithm
-        '''
+        """
         if a == 0:
             return b, 0, 1
         gcd, x1, y1 = extended_gcd(b % a, a)
@@ -58,54 +58,42 @@ def multiplicative_inverse(e: int, phi: int) -> int:
     return x % phi
 
 
-def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
-    '''
+def generate_keypair(
+    p: int, q: int
+) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
+    """
     Generate a public and private keypair.
-    '''
+    """
     if not (is_prime(p) and is_prime(q)):
         raise ValueError("Both numbers must be prime.")
     elif p == q:
         raise ValueError("p and q cannot be equal")
 
-    # n = pq
     n = p * q
 
-    # phi = (p-1)(q-1)
     phi = (p - 1) * (q - 1)
 
-    # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
 
-    # Use Euclid's Algorithm to verify that e and phi(n) are coprime
     g = gcd(e, phi)
     while g != 1:
         e = random.randrange(1, phi)
         g = gcd(e, phi)
 
-    # Use Extended Euclid's Algorithm to generate the private key
     d = multiplicative_inverse(e, phi)
 
-    # Return public and private keypair
-    # Public key is (e, n) and private key is (d, n)
     return ((e, n), (d, n))
 
 
 def encrypt(pk: tp.Tuple[int, int], plaintext: str) -> tp.List[int]:
-    # Unpack the key into it's components
     key, n = pk
-    # Convert each letter in the plaintext to numbers based on
-    # the character using a^b mod m
     cipher = [(ord(char) ** key) % n for char in plaintext]
-    # Return the array of bytes
     return cipher
 
 
 def decrypt(pk: tp.Tuple[int, int], ciphertext: tp.List[int]) -> str:
-    # Unpack the key into its components
     key, n = pk
-    # Generate the plaintext based on the ciphertext and key using a^b mod m
     plain = [chr((char**key) % n) for char in ciphertext]
-    # Return the array of bytes as a string
     return "".join(plain)
 
 
