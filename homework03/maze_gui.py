0ae5f5fe4efd067a0c0080967a7b735ca44b7d97
veronika-sh127 <ronilodge01@gmail.com>
2025-12-02 18:47:39 +0300

    Поправлен файл maze_gui

--- homework03/maze_gui.py
+++ homework03/maze_gui.py
@@ -1,7 +1,8 @@
 import tkinter as tk
-from typing import List
 from tkinter import messagebox, ttk
-from maze import bin_tree_maze, solve_maze, add_path_to_grid
+from typing import List
+
+from maze import add_path_to_grid, bin_tree_maze, solve_maze
 
 
 def draw_cell(x, y, color, size: int = 10):
@@ -16,9 +17,9 @@ def draw_maze(grid: List[List[str | int]], size: int = 10):
     for x, row in enumerate(grid):
         for y, cell in enumerate(row):
             if cell == " ":
-                color = 'White'
+                color = "White"
             elif cell == "■":
-                color = 'black'
+                color = "black"
             elif cell == "X":
                 color = "pink"
             draw_cell(y, x, color, size)
@@ -54,4 +55,3 @@ if __name__ == "__main__":
     ttk.Button(window, text="Solve", command=show_solution).pack(pady=20)
 
     window.mainloop()
-
