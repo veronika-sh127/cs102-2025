7c45083635996752bdff88d1506ac29089983826
veronika-sh127 <ronilodge01@gmail.com>
2025-12-02 18:45:40 +0300

    Замена цвета и вывода

--- homework03/maze_gui.py
+++ homework03/maze_gui.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from typing import List
-from tkinter import ttk, messagebox
+from tkinter import messagebox, ttk
 from maze import bin_tree_maze, solve_maze, add_path_to_grid
 
 
@@ -12,7 +12,7 @@ def draw_cell(x, y, color, size: int = 10):
     canvas.create_rectangle(x, y, x1, y1, fill=color)
 
 
-def draw_maze(grid: List[List[str]], size: int = 10):
+def draw_maze(grid: List[List[str | int]], size: int = 10):
     for x, row in enumerate(grid):
         for y, cell in enumerate(row):
             if cell == " ":
@@ -20,17 +20,20 @@ def draw_maze(grid: List[List[str]], size: int = 10):
             elif cell == "■":
                 color = 'black'
             elif cell == "X":
-                color = "red"
+                color = "pink"
             draw_cell(y, x, color, size)
 
 
 def show_solution():
+    global GRID, N, M
     maze, path = solve_maze(GRID)
     maze = add_path_to_grid(GRID, path)
     if path:
         draw_maze(maze, CELL_SIZE)
     else:
-        tk.messagebox.showinfo("Message", "No solutions")
+        GRID = bin_tree_maze(N, M)
+        maze, path = solve_maze(GRID)
+        draw_maze(maze, CELL_SIZE)
 
 
 if __name__ == "__main__":
@@ -41,7 +44,7 @@ if __name__ == "__main__":
     GRID = bin_tree_maze(N, M)
 
     window = tk.Tk()
-    window.title('Maze')
+    window.title("Maze")
     window.geometry("%dx%d" % (M * CELL_SIZE + 100, N * CELL_SIZE + 100))
 
     canvas = tk.Canvas(window, width=M * CELL_SIZE, height=N * CELL_SIZE)
